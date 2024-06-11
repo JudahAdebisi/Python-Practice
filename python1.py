55e987e9f99a1c12662ea5ace02f5dc0b33bfd19
JudahAdebisi <Judahadebisi1@gmail.com>
2024-06-11 05:28:40 -0500

    This is my first python project

---
+++ python1.py
@@ -0,0 +1,17 @@
+"""
+if 2 < 5:
+ print("Five is greater than two!")
+
+
+#This is a second script 
+if 7 > 4:
+ print(" Four is less than seven!")
+"""
+
+#I give x to be equal to 5
+#y to be equal to my learning
+#to explain the result
+x= 5
+y= "Judah is learning python"
+print(x)
+print (y)
